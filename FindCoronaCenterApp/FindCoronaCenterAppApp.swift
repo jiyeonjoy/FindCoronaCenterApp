d2194d4a18d54e68570ebdc14a4bc7da7e4fe4dc
최지연 <choi02647@naver.com>
2023-06-02 12:33:45 +0900

    Initial Commit

---
+++ FindCoronaCenterApp/FindCoronaCenterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindCoronaCenterAppApp.swift
+//  FindCoronaCenterApp
+//
+//  Created by 최지연/클라이언트 on 2023/06/02.
+//
+
+import SwiftUI
+
+@main
+struct FindCoronaCenterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
