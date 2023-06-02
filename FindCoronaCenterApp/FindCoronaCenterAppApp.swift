c068f719bf01412d73a62857b373120f488f19be
최지연 <choi02647@naver.com>
2023-06-02 13:24:53 +0900

    done ui

--- FindCoronaCenterApp/FindCoronaCenterAppApp.swift
+++ FindCoronaCenterApp/FindCoronaCenterAppApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct FindCoronaCenterAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SelectRegionView()
         }
     }
 }
---
+++ FindCoronaCenterApp/Network/CenterNetwork.swift
@@ -0,0 +1,49 @@
+//
+//  CenterNetwork.swift
+//  FindCoronaCenterApp
+//
+//  Created by 최지연/클라이언트 on 2023/06/02.
+//
+
+import Foundation
+import Combine
+
+class CenterNetwork {
+    private let session: URLSession
+    let api = CenterAPI()
+    
+    init(session: URLSession = .shared) {
+        self.session = session
+    }
+    
+    func getCenterList() -> AnyPublisher<[Center], URLError> {
+        guard let url = api.getCenterListComponents().url else {
+            return Fail(error: URLError(.badURL)).eraseToAnyPublisher()
+        }
+        
+        var request = URLRequest(url: url)
+        request.setValue("Infuser 3gUbMESKsPcNX1hxbGHzWQPY2uhJ6+d4Y/bPogATMZEwV7OJNfzXbYivSt02upze4G4/VYKFGKY25xgKl6my3g==", forHTTPHeaderField: "Authorization")
+        
+        return session.dataTaskPublisher(for: request)
+            .tryMap { data, response in
+                guard let httpResponse = response as? HTTPURLResponse else {
+                    throw URLError(.unknown)
+                }
+                
+                switch httpResponse.statusCode {
+                case 200..<300:
+                    return data
+                case 400..<500:
+                    throw URLError(.clientCertificateRejected)
+                case 500..<599:
+                    throw URLError(.badServerResponse)
+                default:
+                    throw URLError(.unknown)
+                }
+            }
+            .decode(type: CenterAPIResponse.self, decoder: JSONDecoder())
+            .map { $0.data }
+            .mapError { $0 as! URLError }
+            .eraseToAnyPublisher()
+    }
+}
